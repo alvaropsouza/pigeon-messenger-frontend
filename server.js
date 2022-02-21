948dfad089d54390e19d84861ba661fc738b981e
alvaropsouza <alvaropatricio61@gmail.com>
2022-02-21 16:12:26 -0300

    feat: Added a bunch of new pages

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(express.static("views"));
+app.use(express.static('views'))
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
 
-app.get("/register", function (req, res) {
-  res.sendFile(path.join(__dirname, "/register.html"));
-});
+app.get('/user/loginSuccess', function (req, res) {
+  res.sendFile(path.join(__dirname, '/loginSuccess.html'))
+})
 
-app.get("/forgot", function (req, res) {
-  res.sendFile(path.join(__dirname, "/forgot.html"));
-});
+app.get('/user/loginError', function (req, res) {
+  res.sendFile(path.join(__dirname, '/loginError.html'))
+})
 
-app.listen(port);
-console.log("Server started at http://localhost:" + port);
+app.get('/register', function (req, res) {
+  res.sendFile(path.join(__dirname, '/register.html'))
+})
+
+app.get('/user/chatbox', function (req, res) {
+  res.sendFile(path.join(__dirname, '/chatbox.html'))
+})
+
+app.get('/forgot', function (req, res) {
+  res.sendFile(path.join(__dirname, '/forgot.html'))
+})
+
+app.listen(port)
+console.log('Server started at http://localhost:' + port)
