62207e74f0dd54894932a58f19768c63e02befc5
alvaropsouza <alvaropatricio61@gmail.com>
2022-02-21 09:13:19 -0300

    fix: Rota de login trocada por rota de register

---
+++ formSender.js
@@ -0,0 +1,14 @@
+// var url = "localhost:5050/user/signin";
+
+// postLogin(e) {
+//     e.preventDefault()
+//     document.querySelector('')
+//     $http.post(url, data).then(function (response) {
+//         if (response.data) {
+//           console.log("success");
+//         } else {
+//           console.log("failure");
+//         }
+//       });
+
+// }
