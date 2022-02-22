1082dea1a53c167c62aef73d4ee8c3cbe36ad4c7
alvaropsouza <alvaropatricio61@gmail.com>
2022-02-22 15:39:28 -0300

    fix: remoção pasta node_modules

--- server.js
+++ server.js
@@ -10,20 +10,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get('/user/loginSuccess', function (req, res) {
-  res.sendFile(path.join(__dirname, '/loginSuccess.html'))
-})
-
 app.get('/user/loginError', function (req, res) {
   res.sendFile(path.join(__dirname, '/loginError.html'))
 })
 
 app.get('/register', function (req, res) {
   res.sendFile(path.join(__dirname, '/register.html'))
-})
-
-app.get('/user/chatbox', function (req, res) {
-  res.sendFile(path.join(__dirname, '/chatbox.html'))
 })
 
 app.get('/forgot', function (req, res) {
